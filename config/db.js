0fd8b3d3ed0870386618f879b199bf1ef84a46ac
sanghrakshitwaghmare <113676508+sanghrakshitwaghmare@users.noreply.github.com>
2024-09-21 11:18:49 +0530

    Add files via upload

---
+++ config/db.js
@@ -0,0 +1,17 @@
+const mysql = require("mysql");
+const dotenv = require("dotenv");
+dotenv.config();
+
+const connection = mysql.createConnection({
+    host: process.env.HOST,
+    user: process.env.USER,
+    password: process.env.PASSWORD,
+    database: process.env.DATABASE
+});
+
+connection.connect((error) => {
+    if(error) throw error;
+    console.log("Connected to database");
+})
+
+module.exports = connection;
